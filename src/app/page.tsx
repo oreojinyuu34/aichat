dec68d8816553a6079f630b28a8939aaf3de0723
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-12-09 11:52:15 +0900

    ログアウト時にログイン画面へ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-"use client";
+// "use client";
 
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import { useAppContext } from "@/context/AppContext";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { user } = useAppContext();
+  const router = useRouter();
+
+  if (!user) {
+    router.push("/auth/login");
+  }
 
   return (
     <div className="flex h-screen justify-center items-center">
