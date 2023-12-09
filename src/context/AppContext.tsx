d9b35543c420f213dcccb9539f2091475ee00576
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-12-09 12:06:03 +0900

    auth, router追加

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -60,7 +60,7 @@ export function AppProvider({ children }: AppProviderProps) {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [auth, router]);
 
   return (
     <AppContext.Provider
